fd49c836d2540a55347985174bb5243b46386fc0
Maziar <mfooladi@gmail.com>
2019-10-04 01:30:18 -0400

    sample7

--- EM_Factorial_IOHMM_backup.py
+++ EM_Factorial_IOHMM_backup.py
@@ -6,15 +6,31 @@ import xlsxwriter
 import xlrd
 
 np.set_printoptions(linewidth=600)
-np.set_printoptions(precision=2, edgeitems=5)
+np.set_printoptions(precision=2, edgeitems=25)
 
 
-workbook = xlrd.open_workbook('IO_sample6.xlsx')
+workbook = xlrd.open_workbook('IO_sample7.xlsx')
 worksheet = workbook.sheet_by_index(0)
+
+state_scale = 2
+
+agent_num = 3
+
+input_num = 3
+
+output_num = 8
+
+state_total = state_scale ** agent_num
+
+input_tot = input_num ** agent_num
+
+pi = np.ones((state_total,)) / state_total  # initial distribution
 
 data_sample_i = list()
 
-for i in range(3):
+state_vec = np.arange(1, state_total + 1).reshape((1, state_total))
+
+for i in range(agent_num):
     data_sample_i.append(worksheet.col_values(i))
 
 data_input = np.transpose(np.array(data_sample_i))
@@ -31,22 +47,24 @@ time_length = len(time_seq)
 output_seq = data_output
 output_lambda = dict()  # output_lambda(t) = 1 when the t'th output is l
 
-for i in range(1,9):
+for i in range(1, output_num + 1):
     output_lambda[i] = np.where(output_seq == i)[0]
 
 
 def sigma_input(input_seq, t_len):
     input_k = dict()
 
-    for i1 in range(6):
-        for i2 in range(6):
-            for i3 in range(6):
-                input_k[i1 / 5, i2 / 5, i3 / 5] = []
+    for i1 in range(1, input_num + 1):
+        for i2 in range(1, input_num + 1):
+            for i3 in range(1, input_num + 1):
+                input_k[i1, i2, i3] = []
+                
                 for k in range(t_len):
-                    if list(input_seq[k]) == [i1 / 5, i2 / 5, i3 / 5]:
-                        input_k[i1 / 5., i2 / 5., i3 / 5.].append(1)
+                    if list(input_seq[k]) == [i1, i2, i3]:
+                        input_k[i1, i2, i3].append(1)
+                
                     else:
-                        input_k[i1 / 5., i2 / 5., i3 / 5.].append(0)
+                        input_k[i1, i2, i3].append(0)
 
     return input_k
 
@@ -67,62 +85,47 @@ for i, ti in enumerate(input_lambda):
 
 #####################################################################
 # plot input sequence
-plt.subplots(1, 1, sharex='all', sharey='all')
-plt.subplot(211)
+# plt.subplots(1, 1, sharex='all', sharey='all')
+# plt.subplot(211)
+#
+# for i, u_t in enumerate(np.transpose(input_seq)):
+#     plt.plot(time_seq, u_t)
+#     plt.grid(color='b', axis='y')
 
-for i, u_t in enumerate(np.transpose(input_seq)):
-    plt.plot(time_seq, u_t)
-    plt.grid(color='b', axis='y')
-
-plt.subplot(212)
-plt.plot(time_seq, output_seq, '.')
+# plt.subplot(212)
+# plt.plot(time_seq, output_seq, '.')
 
 # plt.show()
 #####################################################################
 
 # w_transition = [w_b, w_s, w_x1, w_x2, w_x3]
 w_transition = np.ndarray((8, 5))
-w_transition[0, :] = [.1, -4, 4, 4, 4]
-w_transition[1, :] = [8, -4, -8, 4, 4]
-w_transition[2, :] = [8, -4, 4, -8, 4]
-w_transition[3, :] = [8, -4, 4, 4, -8]
-w_transition[4, :] = [-16, 4, -8, -8, 8]
-w_transition[5, :] = [-16, 4, -8, 8, -8]
-w_transition[6, :] = [-16, 4, 8, -8, -8]
-w_transition[7, :] = [-8, 2, -4, -4, -4]
+w_transition[0, :] = [.1, -4, 4/3, 4/3, 4/3]
+w_transition[1, :] = [8, -4, -8/3, 4/3, 4/3]
+w_transition[2, :] = [8, -4, 4/3, -8/3, 4/3]
+w_transition[3, :] = [8, -4, 4/3, 4/3, -8/3]
+w_transition[4, :] = [-16, 4, -8/3, -8/3, 8/3]
+w_transition[5, :] = [-16, 4, -8/3, 8/3, -8/3]
+w_transition[6, :] = [-16, 4, 8/3, -8/3, -8/3]
+w_transition[7, :] = [-8, 2, -4/3, -4/3, -4/3]
 
 w_observation = np.ndarray((8, 5))
-w_observation[0, :] = [6, -4, 1, 1, 1]
-w_observation[1, :] = [12, -4, -4, 1, 1]
-w_observation[2, :] = [16, -4, 1, -4, 1]
-w_observation[3, :] = [20, -4, 1, 1, -4]
-w_observation[4, :] = [-17, 4, -4, -4, 4]
-w_observation[5, :] = [-21, 4, -4, 4, -4]
-w_observation[6, :] = [-25, 4, 4, -4, -4]
-w_observation[7, :] = [-25, 4, -4, -4, -4]
+w_observation[0, :] = [6, -4, 1/3, 1/3, 1/3]
+w_observation[1, :] = [12, -4, -4/3, 1/3, 1/3]
+w_observation[2, :] = [16, -4, 1/3, -4/3, 1/3]
+w_observation[3, :] = [20, -4, 1/3, 1/3, -4/3]
+w_observation[4, :] = [-17, 4, -4/3, -4/3, 4/3]
+w_observation[5, :] = [-21, 4, -4/3, 4/3, -4/3]
+w_observation[6, :] = [-25, 4, 4/3, -4/3, -4/3]
+w_observation[7, :] = [-25, 4, -4/3, -4/3, -4/3]
 
-state_scale = 2
-
-agent_num = 3
-
-input_num = 6
-
-output_num = 8
-
-state_total = state_scale ** agent_num
-
-input_tot = input_num ** agent_num
-
-pi = np.ones((state_total,)) / state_total  # initial distribution
-
-state_vec = np.arange(1, state_total + 1).reshape((1, state_total))
 
 
 def mlogit_transition(w, u):
     x_matrix = np.ones((1, state_total))
     y_matrix = state_vec
     z_matrix = np.concatenate((x_matrix, y_matrix))
-    a = np.ones((state_total, 3))
+    a = np.ones((state_total, agent_num))
     E_matrix = []
 
     for t, u_t in enumerate(u):
@@ -134,8 +137,16 @@ def mlogit_transition(w, u):
         except:
             pass
 
-    a_ijt = np.ones((state_total, state_total)) / state_total
-
+    # a_ijt = np.ones((state_total, state_total)) / state_total
+    a_ijt = np.array([[.24, .15, .15, .15, .1, .1, .1, .01],
+                      [.2, .24, .1, .1, .15, .15, .05, .01],
+                      [.2, .1, .24, .1, .15, .05, .15, .01],
+                      [.2, .1, .1, .24, .05, .15, .15, .01],
+                      [.06, .15, .15, .05, .34, .1, .1, .05],
+                      [.06, .15, .05, .15, .1, .34, .1, .05],
+                      [.06, .05, .15, .15, .1, .1, .34, .05],
+                      [.01, .1, .1, .1, .15, .15, .15, .24]])
+    
     for t in range(len(E_matrix)):
         a_ij = np.empty((1, state_total))
 
@@ -173,7 +184,7 @@ def mlogit_observation(w, output, input):
         except:
             pass
 
-    b_jt = np.ones((1, 8))
+    b_jt = np.ones((1, state_total))
 
     for t in range(len(E_matrix)):
         b_ij = np.empty((1, state_total))
@@ -231,7 +242,7 @@ def forward(params):
                 else:
                     alpha[k, j] += alpha[k - 1, i] * A[k, i, j] * O[k, j]
 
-    return alpha, max(np.sum(alpha[N - 1, :]), 10 ** -250)
+    return alpha, max(np.sum(alpha[N - 1, :]), 10 ** -300)
 
 
 def backward(params):
@@ -250,7 +261,7 @@ def backward(params):
             for j in range(S):
                 beta[k, i] += beta[k + 1, j] * A[k + 1, i, j] * O[k + 1, j]
 
-    return beta, max(np.sum(pi * O[0] * beta[0, :]), 10 ** -250)
+    return beta, max(np.sum(pi * O[0] * beta[0, :]), 10 ** -300)
 
 
 def baum_welch(output_seq, pi, iterations, input_seq, w_transition, w_obs):
@@ -275,15 +286,15 @@ def baum_welch(output_seq, pi, iterations, input_seq, w_transition, w_obs):
         alpha, za = forward((pi, A, O))
         beta, zb = backward((pi, A, O))
         # print('alpha\n', alpha)
-        print('za\n', za)
+        # print('za\n', za)
         # print('beta\n', beta)
-        print('zb\n', zb)
+        # print('zb\n', zb)
 
         assert abs(za - zb) < 1e-2, "it's badness 10000 if the marginals don't agree"
 
         # M-step here, calculating the frequency of starting state, transitions and (state, obs) pairs
         pi1 += alpha[0, :] * beta[0, :] / za
-        pi = pi1 / max(np.sum(pi1), 10 ** -250)  # normalise pi1
+        pi = pi1 / np.sum(pi1)  # normalise pi1
 
         for k in range(0, obs_length):
             O1[k] += alpha[k, :] * beta[k, :] / za
@@ -294,20 +305,20 @@ def baum_welch(output_seq, pi, iterations, input_seq, w_transition, w_obs):
                     A1[k - 1, i, j] = alpha[k - 1, i] * A[k, i, j] * O[k, j] * beta[k, j] / za
 
         for k, u in enumerate(input_lambda.values()):
-            H = np.zeros_like(A[0]) + 10 ** -250
+            H = np.zeros_like(A[0]) + 10 ** -300
             if len(u) > 0:
                 for i, t in enumerate(u):
                     H += A1[t]
-                    if np.sum(H) > 0:
+                    # if np.sum(H) > 0:
 
                         # print('np.sum(H, 1)')
                         # print(np.sum(H, 1))
                         # print('H')
                         # print(H)
-                        H1[t] = np.transpose(np.transpose(H) / np.sum(H, 1))
+                    H1[t] = np.transpose(np.transpose(H) / np.sum(H, 1))
 
         OM1 = np.zeros_like(O)
-        w_ilk = np.zeros((input_tot, output_num, state_total)) + 10 ** -250
+        w_ilk = np.zeros((input_tot, output_num, state_total)) + 10 ** -300
 
         for i, ti in enumerate(input_lambda):
             for o, to in enumerate(output_lambda):
