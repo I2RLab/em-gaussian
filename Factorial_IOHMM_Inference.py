f16b10d97cd6d2109ce9c2ca35189f62b6066b72
mfooladi <mfooladi@gmail.com>
2019-10-07 19:19:57 -0400

    up

--- Factorial_IOHMM_Inference.py
+++ Factorial_IOHMM_Inference.py
@@ -12,7 +12,7 @@ from mpl_toolkits.mplot3d import Axes3D
 import  EM_Factorial_IOHMM_backup as EM
 
 np.set_printoptions(linewidth=520)
-np.set_printoptions(precision=3, edgeitems=5)
+np.set_printoptions(precision=3, edgeitems=15)
 
 # constants
 
@@ -72,9 +72,7 @@ def io_index_func(i_index, y_data):
 
 input_sequence = i_index_func(data_input)
 
-pi_trained, A_trained, O_trained, A_ijk, O_jlk = EM.baum_welch(EM.output_seq, EM.pi, 5, EM.input_seq, EM.w_transition, EM.w_observation)
-
-
+pi_trained, A_trained, O_trained, A_ijk, O_jlk = EM.baum_welch(EM.output_seq, EM.pi, 15, EM.input_seq, EM.w_transition, EM.w_observation)
 
 # transition joint probability P(u_n, s_n-1, s_n)
 # A_iju = dict()
@@ -104,14 +102,12 @@ y = np.ones((len(data_input),))  # y in {1,2,3,4,5,6,7,8}
 
 # initial belief
 bel0 = np.ones((state_num,))
-# bel0 = np.ones((state_num,)) * np.array([0.1, 0.1, 0.1, 0.2, 0.2, 0.1, 0.1, 0.1])
 
 belief = np.zeros((len(data_input) + 1, state_num))
 
 belief[0] = bel0
 
 for t in range(len(data_input)):
-    # print('t=', t)
     print('A_ijk[input_sequence[{}]]'.format(t))
     print(A_ijk[input_sequence[t]])
     # print('belief[{}]'.format(t))
@@ -139,7 +135,7 @@ for t in range(len(data_input)):
 print('belief')
 print(belief)
 
-
+# PLOT RESULTS #
 fig = plt.figure()
 ax1 = fig.add_subplot(111, projection='3d')
 
@@ -159,52 +155,9 @@ num_elements = len(xpos)
 dx = np.ones(1)
 dy = np.ones(1)
 
-ax1.bar3d(xpos, ypos, zpos, dx, dy, dz, color='#00ceaa')
+colors = plt.cm.jet(np.asanyarray(dz).flatten() / float(np.asanyarray(dz).max()))
+
+ax1.bar3d(xpos, ypos, zpos, dx, dy, dz, color=colors)
 plt.show()
 
 
-
-### plot the data ############################################################
-# X, Y = np.meshgrid(np.arange(0, K, 1), vec_t)
-# Z1 = np.asarray(Bel_T1)
-# Z2 = np.asarray(Bel_T2)
-# max_bel = 0.
-# min_bel = 0.
-# for i in range(len(Z1)):
-#     if max_bel < max(Z1[i]):
-#         max_bel = max(Z1[i])
-#     if min_bel > min(Z1[i]):
-#         min_bel = min(Z1[i])
-# data1 = Z1
-# data2 = Z2
-# ypos1, xpos1 = np.indices(data1.shape)
-# ypos2, xpos2 = np.indices(data2.shape)
-# xpos1 = xpos1.flatten()
-# xpos2 = xpos2.flatten()
-# ypos1 = ypos1.flatten()
-# ypos2 = ypos2.flatten()
-# zpos1 = np.zeros(xpos1.shape)
-# zpos2 = np.zeros(xpos2.shape)
-#
-# fig1 = plt.figure(1)
-# ax = Axes3D(fig1)
-# ax.view_init(elev=90., azim=-90.)
-# colors = plt.cm.jet(data1.flatten() / float(data1.max()))
-# ax.bar3d(xpos1, ypos1, zpos1, .8, .8, data1.flatten(), color=colors)
-# ax.set_xlabel('Time')
-# ax.set_ylabel('Trust')
-#
-# fig2 = plt.figure(2)
-# ax2 = Axes3D(fig2)
-# ax2.view_init(elev=90., azim=-90.)
-# colors = plt.cm.jet(data2.flatten() / float(data2.max()))
-# ax2.bar3d(xpos2, ypos2, zpos2, .8, .8, data2.flatten(), color=colors)
-# ax2.set_xlabel('Trust')
-# ax2.set_ylabel('Time')
-#
-# fig3, (axp1, axp2, axvar1, axvar2) = plt.subplots(4, 1)
-# axp1.plot(np.arange(K), p1)
-# axp2.plot(np.arange(K), p2)
-# axvar1.plot(np.arange(K), MDS1)
-# axvar2.plot(np.arange(K), MDS2)
-# plt.show()
