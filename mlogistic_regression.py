7470e561b064312c0b270baece2d5ad413dca573
mfooladi <mfooladi@gmail.com>
2019-10-17 18:48:52 -0400

    multinomial distribution

--- mlogistic_regression.py
+++ mlogistic_regression.py
@@ -9,30 +9,20 @@ np.set_printoptions(precision=3, edgeitems=25)
 agent_num = 3
 state_scale = 3
 input_num = 3
-output_num = 2
 state_total = state_scale ** agent_num
 input_total = input_num ** agent_num
-output_total = 4
 
 state_vec = np.arange(1, state_total + 1).reshape((1, state_total))
 
 workbook_weights = xlrd.open_workbook('mlogistic_weights_0.xlsx')
 worksheet_weights_transition = workbook_weights.sheet_by_index(0)
-worksheet_weights_observation = workbook_weights.sheet_by_index(1)
 
 weights_transition_read = np.zeros((5, state_total))
-weights_observation_read = np.zeros((4, 2))
 
 for i in range(5):
     weights_transition_read[i, :] = worksheet_weights_transition.col_values(i)
 
-weights_observation_matrix = np.transpose(weights_transition_read)
-
-for i in range(2):
-    weights_observation_read[:, i] = worksheet_weights_observation.col_values(i)
-
-# weights_observation_matrix = np.transpose(weights_observation_read)
-weights_observation_matrix = weights_observation_read
+weights_transition_matrix = np.transpose(weights_transition_read)
 
 workbook_inputs = xlrd.open_workbook('mlogistic_inputs.xlsx')
 
@@ -42,16 +32,12 @@ inputs_read = np.zeros((agent_num, input_total))
 for i in range(agent_num):
     inputs_read[i, :] = worksheet_inputs.col_values(i)
 
-worksheet_outputs = workbook_inputs.sheet_by_index(1)
-output_read = worksheet_outputs.col_values(0)
 
 inputs_matrix = np.transpose(inputs_read)
 
 print('inputs_matrix')
 print(inputs_matrix)
 
-print('output sequence')
-print(output_read)
 
 def mlogit_transition(w, u):
     x_matrix = np.ones((1, state_total))
@@ -92,37 +78,15 @@ def mlogit_transition(w, u):
     A_ijt = a_ijt.reshape((int(len(a_ijt) / state_total), state_total, state_total))
     A_ijt = A_ijt[1::]
 
-    # print(A_ijt)
+    print('A_ijt')
+    print(A_ijt)
     return A_ijt
 
 
-def mlogit_observation(w, y):
-    w_pivot = w[0:-1]
-    o_ljt = np.ndarray((len(y), output_total, state_total))
-    o_lj = np.ndarray((output_total, state_total))
-    
-    for il, l in enumerate(y):
-        for s in range(state_total):
-            o_lj_temp = np.exp(np.matmul(w_pivot, np.array([1, s])))
-            for i in range(output_total - 1):
-                o_lj[i, s] = o_lj_temp[i] / (1 + np.sum(o_lj_temp))
-                
-            o_lj[output_total - 1, s] = 1 / (1 + np.sum(o_lj_temp))
-        
-        o_ljt[il] = o_lj
-        
-    return o_ljt
-        
-
- 
-    
- 
-o_ljt = mlogit_observation(weights_observation_matrix, output_read)
-
 transition_probability = mlogit_transition(weights_transition_matrix, inputs_matrix)
 
-for i in range(len(transition_probability)):
-    print('input=%2d max= %4.3f, min= %4.3f' % (i+1, np.max(transition_probability[i]), np.min(transition_probability[i])))
+# for i in range(len(transition_probability)):
+#     print('input=%2d max= %4.3f, min= %4.3f' % (i+1, np.max(transition_probability[i]), np.min(transition_probability[i])))
 
 
 # PLOT RESULTS #
