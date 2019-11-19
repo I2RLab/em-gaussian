67bc72c7d0d2e6bbca86dc63c3c5077be8321a33
mfooladi <mfooladi@gmail.com>
2019-11-19 16:27:00 -0500

    larger dataset

--- Test_Dataset_Generator.py
+++ Test_Dataset_Generator.py
@@ -1,4 +1,5 @@
 import numpy as np
+import matplotlib.pyplot as plt
 
 
 class TrainingData:
@@ -6,26 +7,46 @@ class TrainingData:
         self.input_output_dict = dict()
         self.input_num_units = 11
         self.input_training = []
-        self.ouput_training = []
+        self.output_training = []
 
     def io_sequence_generator(self):
-        self.input_training = np.array([[10, 10, 10], [10, 10, 9.], [10, 10, 8.], [10, 10, 1.], [10, 9, 1], [10, 8, 7], [10, 8, 8], [10, 10, 10],
-                                        [10, 10, 1.], [10, 10, 1.], [10, 10, 1.], [10, 10, 1.], [10, 10, 1.], [10, 10, 1.], [10, 10, 1.], [10, 10, 1.],
-                                        [8., 10, 4.], [1., 10, 5.], [1., 10, 5.], [1., 10, 5.], [1., 10, 6.], [2., 10, 7.], [3., 10, 8.], [4., 10, 9.],
-                                        [5., 10, 10], [5., 10, 9.], [5., 10, 10], [8., 10, 10], [8., 10, 10], [8., 10, 10], [8., 10, 10], [9., 10, 10],
-                                        [10, 1., 10], [10, 1., 10], [10, 1., 10], [10, 1., 10], [10, 5., 10], [10, 4., 10], [10, 4., 10], [10, 6., 10],])
+        self.input_training = np.array([[10, 10, 10], [10, 10, 9.], [10, 10, 8.], [10, 10, 8.], [10, 9., 8.], [10, 9., 7.], [10, 9., 6.], [10, 8., 6.],
+                                        [10, 10, 6.], [10, 10, 6.], [10, 10, 6.], [10, 10, 4.], [10, 10, 4.], [10, 10, 2.], [10, 10, 1.], [10, 10, 1.],
+                                        [1., 10, 10], [1., 10, 9.], [1., 10, 8.], [1., 10, 8.], [1., 10, 7.], [2., 10, 7.], [4., 10, 9.], [4., 10, 9.],
+                                        [10, 3., 8.], [9., 5., 8.], [10, 3., 9.], [10, 5., 9.], [10, 4., 10], [10, 4., 10], [10, 3., 10], [9., 1., 10],
+                                        [10, 8., 10], [10, 9., 10], [4., 10, 10], [4., 10, 10], [4., 9., 10], [10, 1., 10], [10, 2., 10], [10, 1., 10]])
 
-        self.ouput_training = np.array([[1], [1], [1], [2], [1], [1], [1], [1],
-                                        [4], [4], [4], [4], [4], [4], [4], [4],
-                                        [4], [2], [2], [2], [2], [2], [2], [2],
-                                        [2], [1], [1], [2], [2], [2], [1], [1],
-                                        [3], [3], [3], [3], [3], [3], [3], [3]])
+        self.output_training = np.array([[1], [1], [1], [1], [1], [1], [1], [1],
+                                         [4], [4], [4], [4], [4], [4], [4], [4],
+                                         [2], [2], [2], [2], [2], [2], [2], [2],
+                                         [3], [3], [3], [3], [3], [3], [3], [3],
+                                         [1], [1], [1], [2], [2], [2], [3], [3]])
 
-        return np.array(self.input_training), np.array(self.ouput_training)
+
+        ax1 = plt.subplot(211)
+        plt.plot(self.input_training[:,0], "X")
+        plt.plot(self.input_training[:,1], "v")
+        plt.plot(self.input_training[:,2], "P")
+        plt.ylabel('Performance level')
+        ax1.set_xticks(np.arange(0, 40, 1))
+        ax1.set_yticks(np.arange(1, 11, 1))
+        plt.grid(markevery=3)
+        plt.legend(('Y = 2', 'Y = 3', 'Y = 4'))
+
+        ax2 = plt.subplot(212)
+        plt.plot(self.output_training)
+        plt.ylabel('Mode')
+        plt.xlabel('time')
+        ax2.set_yticks(np.arange(1,5,1))
+        plt.grid()
+        plt.savefig('test_data_plot.png')
+        plt.show()
+
+
+        return np.array(self.input_training), np.array(self.output_training)
 
 
 if __name__ == '__main__':
-
     TD = TrainingData()
     [i, o] = TD.io_sequence_generator()
     print(i, '\n', o)
--- Training_Dataset_Generator.py
+++ Training_Dataset_Generator.py
@@ -1,40 +1,64 @@
 import numpy as np
-
+import matplotlib.pyplot as plt
 
 class TrainingData:
     def __init__(self):
         self.input_output_dict = dict()
         self.input_num_units = 11
         self.input_training = []
-        self.ouput_training = []
+        self.output_training = []
 
     def io_sequence_generator(self):
         t = 0
+
         for i1 in range(1, self.input_num_units):
             for i2 in range(1, self.input_num_units):
                 for i3 in range(1, self.input_num_units):
                     u = [i1, i2, i3]
                     indexes = [(i, x) for i, x in enumerate(u) if x == min(u)]
-                    # print(u, indexes)
+
                     for i_r in indexes:
                         if i_r[1] < 8:
                             self.input_output_dict[t, i1, i2, i3] = i_r[0] + 2
                             self.input_training.append([i1, i2, i3])
                             self.input_training.append([i1, i2, i3])
-                            self.ouput_training.append([i_r[0] + 2])
-                            self.ouput_training.append([i_r[0] + 2])
+                            self.input_training.append([i1, i2, i3])
+                            self.output_training.append([i_r[0] + 2])
+                            self.output_training.append([i_r[0] + 2])
+                            self.output_training.append([i_r[0] + 2])
                         else:
                             self.input_output_dict[t, i1, i2, i3] = 1
                             self.input_training.append([i1, i2, i3])
                             self.input_training.append([i1, i2, i3])
-                            self.ouput_training.append([1])
-                            self.ouput_training.append([1])
+                            self.input_training.append([i1, i2, i3])
+                            self.output_training.append([1])
+                            self.output_training.append([1])
+                            self.output_training.append([1])
 
                         t += 1
+        '''
+        # plot data
+        ax1 = plt.subplot(211)
+        plt.plot(self.input_training[:], "X")
+        # plt.plot(self.input_training[1][:], "v")
+        # plt.plot(self.input_training[2][:], "P")
+        plt.ylabel('Performance level')
+        # ax1.set_xticks(np.arange(0, 40, 1))
+        # ax1.set_yticks(np.arange(1, 11, 1))
+        plt.grid(markevery=3)
+        plt.legend(('Y = 2', 'Y = 3', 'Y = 4'))
 
-        # print('input_output_dict')
-        # print(self.input_output_dict)
-        return np.array(self.input_training), np.array(self.ouput_training)
+        ax2 = plt.subplot(212)
+        plt.plot(self.output_training)
+        plt.ylabel('Mode')
+        plt.xlabel('time')
+        ax2.set_yticks(np.arange(1, 5, 1))
+        plt.grid()
+
+        plt.show()
+        '''
+
+        return np.array(self.input_training), np.array(self.output_training)
 
 
 if __name__ == '__main__':
